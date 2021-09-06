04426f34a06928fab680338fad090b9292b16b9a
Lily Finley <TechnoFairyGirl@users.noreply.github.com>
2021-09-06 07:32:56 -0500

    Initial code commit.

---
+++ CycladesPdu.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CycladesPduApi
+{
+	class CycladesPdu
+	{
+		public static T Retry<T>(int times, Func<int, T> func)
+		{
+			for (var i = 1; ; i++)
+			{
+				try { return func(i); }
+				catch { if (times > 0 && i >= times) throw; }
+			}
+		}
+
+		public static void Retry(int times, Action<int> func) =>
+			Retry(times, i => { func(i); return 0; });
+
+		public string SerialPort { get; }
+		public string User { get; }
+		public string Password { get; }
+
+		public CycladesPdu(string serialPort, string user = "admin", string password = "pm8")
+		{
+			SerialPort = serialPort;
+			User = user;
+			Password = password;
+		}
+
+		Task commandDelay = Task.CompletedTask;
+
+		public string[] DoCommand(string command, out int outletCount)
+		{
+			try
+			{
+				commandDelay.Wait();
+
+				using var port = new SerialPort(SerialPort);
+
+				port.DtrEnable = true;
+				port.RtsEnable = true;
+
+				port.ReadTimeout = 5000;
+
+				string[] ReadLinesTo(string str)
+				{
+					port.NewLine = "\r\n";
+					return port.ReadTo($"\r\n{str}").Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+				}
+
+				void WriteLine(string str)
+				{
+					port.NewLine = "\r";
+					port.WriteLine(str);
+				}
+
+				string[] WriteCommand(string cmd, string delimiter = "pm>")
+				{
+					WriteLine(cmd);
+					return ReadLinesTo(delimiter);
+				}
+
+				port.Open();
+
+				var banner = ReadLinesTo("Username: ");
+				var lastOutlet = int.Parse(banner[4].Split(' ').Last());
+
+				outletCount = lastOutlet;
+
+				if (command == null) return null;
+
+				WriteCommand(User, "Password: ");
+				WriteCommand(Password);
+
+				var waitForChainsTimeout = Task.Delay(20000);
+
+				while (true)
+				{
+					if (waitForChainsTimeout.IsCompleted) throw new TimeoutException();
+					var status = WriteCommand($"status {lastOutlet}").Last();
+					if (status.Contains("ON") || status.Contains("OFF")) break;
+				}
+
+				var result = WriteCommand(command);
+
+				return result;
+			}
+			finally { commandDelay = Task.Delay(1000); }
+		}
+
+		public string[] DoCommand(string command) => DoCommand(command, out _);
+
+		public bool GetOutletState(int outlet)
+		{
+			if (outlet < 1) throw new ArgumentOutOfRangeException("outlet");
+			var result = DoCommand($"status {outlet}", out var outletCount);
+			if (outlet > outletCount) throw new ArgumentOutOfRangeException("outlet");
+
+			foreach (var line in result)
+			{
+				var match = Regex.Match(line, @"\t[^\t]*(ON|OFF)\t");
+				if (!match.Success) continue;
+
+				return match.Groups[1].Value == "ON";
+			}
+
+			throw new InvalidOperationException();
+		}
+
+		public void SetOutletState(int outlet, bool state)
+		{
+			string onOff = state ? "on" : "off";
+
+			if (outlet < 1) throw new ArgumentOutOfRangeException("outlet");
+			var result = DoCommand($"{onOff} {outlet}", out var outletCount);
+			if (outlet > outletCount) throw new ArgumentOutOfRangeException("outlet");
+
+			if (!result.Any(line => line.EndsWith($"Outlet turned {onOff}.")))
+				throw new InvalidOperationException();
+		}
+
+		public int GetOutletCount()
+		{
+			DoCommand(null, out var outletCount);
+			return outletCount;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using SimpleHttp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CycladesPduApi
+{
+	class Config
+	{
+		public class PortMapping
+		{
+			public string Endpoint { get; set; }
+			public string SerialDevice { get; set; }
+		}
+
+		public int HttpPort { get; set; }
+		public string HttpToken { get; set; }
+		public PortMapping[] Ports { get; set; }
+	}
+
+	static class Program
+	{
+		static Config config;
+
+		static void Main(string[] args)
+		{
+			var exePath = System.Reflection.Assembly.GetEntryAssembly().Location;
+			var configPath = args.Length >= 1 ? args[0] : Path.Combine(Path.GetDirectoryName(exePath), "config.json");
+			config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(configPath));
+
+			var portMappings = config.Ports.ToDictionary(
+				portMapping => portMapping.Endpoint,
+				portMapping => new CycladesPdu(portMapping.SerialDevice));
+
+			var server = new HttpServer(config.HttpPort);
+
+			foreach (var portMapping in portMappings)
+			{
+				var endpoint = portMapping.Key;
+				var pdu = portMapping.Value;
+
+				server.AddExactRoute("GET", $"/{endpoint}/outlets", (request, response) =>
+				{
+					if (request.Headers["authorization"] != $"Bearer {config.HttpToken}") 
+						throw new UnauthorizedAccessException();
+
+					lock (pdu) response.WriteBodyJson(CycladesPdu.Retry(10, i => pdu.GetOutletCount()));
+				});
+
+				server.AddRoute("GET", $@"/{endpoint}/outlet/(\d+)", (args, request, response) =>
+				{
+					if (request.Headers["authorization"] != $"Bearer {config.HttpToken}")
+						throw new UnauthorizedAccessException();
+
+					var outlet = int.Parse(args[0]);
+					lock (pdu) response.WriteBodyJson(CycladesPdu.Retry(10, i => pdu.GetOutletState(outlet)));
+				});
+
+				server.AddRoute("POST", $@"/{endpoint}/outlet/(\d+)", (args, request, response) =>
+				{
+					if (request.Headers["authorization"] != $"Bearer {config.HttpToken}")
+						throw new UnauthorizedAccessException();
+
+					var outlet = int.Parse(args[0]);
+					var state = request.ReadBodyJson<bool>();
+					lock (pdu) CycladesPdu.Retry(10, i => pdu.SetOutletState(outlet, state));
+
+					server.Log($"Outlet {outlet} turned {(state ? "on" : "off")}.");
+				});
+			}
+
+			server.Start();
+
+			Task.Delay(-1).Wait();
+		}
+	}
+}
